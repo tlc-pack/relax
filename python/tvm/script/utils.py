43f4a4815d6956bc0e5ac53cebeb65e8b9046210
Jared Roesch <jroesch@octoml.ai>
2021-08-12 14:32:02 -0700

    Relax AST (#2)
    Co-authored-by: ZihengJiang <ziheng@apache.org>

--- python/tvm/script/utils.py
+++ python/tvm/script/utils.py
@@ -100,6 +100,7 @@ def buffer_slice_to_region(
 
 def tvm_span_from_synr(span: synr.ast.Span) -> Span:
     """Convert a synr span to a TVM span"""
+    assert isinstance(span, synr.ast.Span), "Expected span to be synr.ast.Span, but got " + str(type(span))
     return Span(
         SourceName(span.filename),
         span.start_line,
@@ -111,6 +112,7 @@ def tvm_span_from_synr(span: synr.ast.Span) -> Span:
 
 def synr_span_from_tvm(span: Span) -> synr.ast.Span:
     """Convert a TVM span to a synr span"""
+    assert isinstance(span, synr.ast.Span), "Expected span to be tvm.ir.Span, but got " + str(type(span))
     return synr.ast.Span(
         span.source_name.name,
         span.line,
