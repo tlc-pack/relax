a5d68daad7ee622c6d6f37ea2d84530dc83982d8
Jared Roesch <jroesch@octoml.ai>
2021-08-12 14:32:02 -0700

    Relax AST (#2)
    Co-authored-by: ZihengJiang <ziheng@apache.org>

---
+++ include/tvm/relax/type.h
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file tvm/relax/type.h
+ * \brief Relax typed AST nodes.
+ */
+#ifndef TVM_RELAX_TYPE_H_
+#define TVM_RELAX_TYPE_H_
+
+#include <tvm/ir/attrs.h>
+#include <tvm/ir/env_func.h>
+#include <tvm/ir/tensor_type.h>
+#include <tvm/ir/type.h>
+#include <tvm/ir/type_relation.h>
+#include <tvm/runtime/registry.h>
+#include <tvm/tir/expr.h>
+
+#include <string>
+
+namespace tvm {
+namespace relax {
+
+class ShapeTypeNode : public TypeNode {
+ public:
+
+  void VisitAttrs(tvm::AttrVisitor* v) {
+  }
+
+  bool SEqualReduce(const ShapeTypeNode* other, SEqualReducer equal) const {
+    return true;
+  }
+
+  void SHashReduce(SHashReducer hash_reduce) const { hash_reduce(0); }
+
+  static constexpr const char* _type_key = "relax.ShapeType";
+  TVM_DECLARE_FINAL_OBJECT_INFO(ShapeTypeNode, TypeNode);
+};
+
+class ShapeType : public Type {
+ public:
+  explicit ShapeType();
+  explicit ShapeType(runtime::ObjectPtr<runtime::Object> n) : Type(n) {}
+  TVM_DEFINE_DEFAULT_COPY_MOVE_AND_ASSIGN(ShapeType);
+  const ShapeTypeNode* operator->() const {
+    return static_cast<const ShapeTypeNode*>(data_.get());
+  }
+  const ShapeTypeNode* get() const {
+    return operator->();
+  }
+  using ContainerType = ShapeTypeNode; 
+};
+
+
+class DynTensorTypeNode : public BaseTensorTypeNode {
+ public:
+  /*!
+   * \brief The rank of the tensor, use -1 to denote dynamic rank tensor.
+   */
+  int rank;
+  /*! \brief The content data type, use void to denote the dtype is unknown. */
+  DataType dtype;
+
+  void VisitAttrs(tvm::AttrVisitor* v) {
+    v->Visit("rank", &rank);
+    v->Visit("dtype", &dtype);
+    v->Visit("span", &span);
+  }
+
+  bool SEqualReduce(const DynTensorTypeNode* other, SEqualReducer equal) const {
+    return equal(rank, other->rank) && equal(dtype, other->dtype);
+  }
+
+  void SHashReduce(SHashReducer hash_reduce) const {
+    hash_reduce(rank);
+    hash_reduce(dtype);
+  }
+
+  static constexpr const char* _type_key = "relax.DynTensorType";
+  TVM_DECLARE_FINAL_OBJECT_INFO(DynTensorTypeNode, BaseTensorTypeNode);
+};
+
+/*!
+ * \brief Managed reference to DynTensorTypeNode.
+ * \sa DynTensorTypeNode.
+ */
+class DynTensorType : public Type {
+ public:
+  /*!
+   * \brief Constructor.
+   * \param shape The shape of the tensor.
+   * \param dtype The runtime dtype of the tensor's elements.
+   */
+  TVM_DLL DynTensorType(int rank, DataType dtype);
+
+  TVM_DEFINE_OBJECT_REF_METHODS(DynTensorType, Type, DynTensorTypeNode);
+};
+
+}  // namespace relax
+}  // namespace tvm
+#endif  // TVM_RELAX_TYPE_H_
---
+++ python/tvm/relax/expr.py
@@ -0,0 +1,118 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from typing import List, Optional, Union, Dict
+import tvm._ffi
+from ..ir.base import Node, Span, SourceName
+from ..relay.base import Id
+from ..tir import PrimExpr
+from . import _ffi_api
+from .. import relay
+
+GlobalVar = relay.GlobalVar
+Expr = relay.Expr
+Type = relay.Type
+const = relay.const
+
+
+@tvm._ffi.register_object("relax.expr.ShapeExpr")
+class ShapeExpr(Expr):
+    values: List[PrimExpr]
+
+    def __init__(self, values: List[PrimExpr]) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.ShapeExpr, values)
+
+
+@tvm._ffi.register_object("relax.expr.Var")
+class Var(Expr):
+    id: Id
+    type_annotation: Optional[Type]
+
+    def __init__(self, name_hint: str,
+                 shape_annotation: Optional[List[Type]] = None,
+                 type_annotation: Optional[Type] = None) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.Var, name_hint,
+                                            shape_annotation,
+                                            type_annotation)
+
+    @property
+    def name_hint(self):
+        """Get name hint of the current var."""
+        name = str(self.vid.name_hint)
+        return name
+
+
+@tvm._ffi.register_object("relax.expr.DataflowVar")
+class DataflowVar(Var):
+    pass
+
+
+@tvm._ffi.register_object("relax.expr.Binding")
+class Binding(Node):
+    pass
+
+
+@tvm._ffi.register_object("relax.expr.MatchShape")
+class MatchShape(Binding):
+    pattern: List[PrimExpr]
+    value: Expr
+
+    def __init__(self, pattern: List[PrimExpr], value: Expr) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.MatchShape, pattern, value)
+
+
+@tvm._ffi.register_object("relax.expr.VarBinding")
+class VarBinding(Binding):
+    var: Var
+    value: Expr
+
+    def __init__(self, var: Var, value: Expr) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.VarBinding, var, value)
+
+
+@tvm._ffi.register_object("relax.expr.BindingBlock")
+class BindingBlock(Node):
+    bindings: List[Binding]
+
+    def __init__(self, bindings: List[Binding]) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.BindingBlock, bindings)
+
+
+@tvm._ffi.register_object("relax.expr.DataflowBlock")
+class DataflowBlock(BindingBlock):
+    pass
+
+
+@tvm._ffi.register_object("relax.expr.SeqExpr")
+class SeqExpr(Expr):
+    blocks: List[BindingBlock]
+    body: Expr
+
+    def __init__(self, blocks: List[BindingBlock], body: Expr) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.SeqExpr, blocks, body)
+
+
+@tvm._ffi.register_object("relax.expr.Function")
+class Function(Expr):
+    name: Optional[GlobalVar]
+    params: List[Var]
+    body: Expr
+    ret_type: Type
+
+    def __init__(self, params: List[Var], body: Expr,
+                 ret_type: Type, name: Optional[GlobalVar] = None) -> None:
+        self.__init_handle_by_constructor__(_ffi_api.Function, name, params,
+                                            body, ret_type)
