6c3e79c99505d7a83011e7c1643f7a6278ea4945
Altan Haan <ahaan@octoml.ai>
2021-09-13 14:06:07 -0700

    Relax IR Parser (#6)
    * Copy jared's frontend
    
    * Remove some extraneous code + add TODOs
    
    * Skeleton AST
    
    * Added more skeleton AST, worked on parsing shape annotations. Something is wrong with span_to_span
    
    * Fix spans
    
    * Type annotations parsing correctly
    
    * some match_shape support
    
    * More bug fixes! Some stuff parses. Importing into tests is messed up. We probably need to restructure this code as well.
    
    * refactor parser and fill out more stubs
    
    * some parser tests
    
    * yolo dataflow
    
    * checkpoint for rebase
    
    * hook up AST
    
    * add inline TIR parsing
    
    * some cleanup
    
    * support call_packed parsing to ExternFunc call
    
    * remove stub ops
    
    * improve docstrings
    
    * address nits
    
    * support coercing tuples to ShapeExpr when possible for call_dps
    
    Co-authored-by: electriclilies <lilyorthsmith@gmail.com>

--- src/relax/op/op.cc
+++ src/relax/op/op.cc
@@ -1,23 +1,23 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
-#include <tvm/relay/op.h>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 #include <tvm/relax/expr.h>
+#include <tvm/relay/op.h>
 
 namespace tvm {
 namespace relax {
@@ -35,8 +35,7 @@ Expr MakeCallDPS(ShapeExpr shape, Expr func, Tuple args) {
   return Call(op, {shape, func, args}, {}, {});
 }
 
-TVM_REGISTER_GLOBAL("relax.op.call_dps")
-.set_body_typed(MakeCallDPS);
+TVM_REGISTER_GLOBAL("relax.op.call_dps").set_body_typed(MakeCallDPS);
 
 // shape_of
 
@@ -51,5 +50,6 @@ Expr MakeShapeOf(Expr expr) {
 
 TVM_REGISTER_GLOBAL("relax.op.shape_of")
 .set_body_typed(MakeShapeOf);
-} // namespace relax
-} // namespace tvm
+
+}  // namespace relax
+}  // namespace tvm
