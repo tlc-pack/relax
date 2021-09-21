32781ef04ff4168b787099f555155ae3afa68eb4
Yuchen Jin <yuchenj@cs.washington.edu>
2021-09-20 17:04:25 -0700

    Shape and type deduction (#7)
    * Shape and type deduction.
    
    * Fix header.
    
    * Add call attrs to the deduce signature.
    
    * Address comments.
    
    * Add DiagnosticContext to IRBuilder and inference signature.
    
    * Fix nits.

--- tests/python/relax/test_irbuilder.py
+++ tests/python/relax/test_irbuilder.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import tvm
 from tvm import tir
 from tvm import relax as rx
 
@@ -27,13 +28,26 @@ def test_dataflow_block():
     x = rx.Var("x", [m, n], dtype0)
     y = rx.Var("y", [n], dtype1)
     ib = rx.IRBuilder()
+
     with ib.dataflow() as df:
         lv0 = ib.emit(rx.op.add(x, y))
+
         assert lv0.name_hint == "lv0"
+        assert lv0.shape[0] == m
+        assert lv0.shape[1] == n
+        assert lv0.checked_type.rank == 2
+        assert lv0.checked_type.dtype == "float16"
+
         lv1 = ib.emit(rx.op.multiply(lv0, y))
         assert lv1.name_hint == "lv1"
         gv0 = ib.emit_output(lv1)
+
         assert gv0.name_hint == "gv0"
+        assert gv0.shape[0] == m
+        assert gv0.shape[1] == n
+        assert gv0.checked_type.rank == 2
+        assert gv0.checked_type.dtype == "float16"
+
     blocks = ib.get_blocks()
     assert len(blocks) == 1
     assert len(blocks[-1].bindings) == 3
@@ -47,6 +61,7 @@ def test_function_single_block():
     x = rx.Var("x", [m, n], dtype0)
     y = rx.Var("y", [n], dtype1)
     ib = rx.IRBuilder()
+
     with ib.function([x, y]):
         with ib.dataflow() as df:
             lv0 = ib.emit(rx.op.add(x, y))
@@ -56,10 +71,15 @@ def test_function_single_block():
             gv0 = ib.emit_output(lv1)
         assert gv0.name_hint == "gv0"
         ib.emit_output(gv0)
+
     func = ib.get()
     assert func.params[0] == x
     assert func.params[1] == y
     assert func.body.body == gv0
+    assert gv0.shape[0] == m
+    assert gv0.shape[1] == n
+    assert gv0.checked_type.rank == 2
+    assert gv0.checked_type.dtype == "float16"
     assert len(func.body.blocks) == 1
     assert len(func.body.blocks[0].bindings) == 3
 
@@ -72,6 +92,7 @@ def test_function_multi_blocks():
     x = rx.Var("x", [m, n], dtype0)
     y = rx.Var("y", [n], dtype1)
     ib = rx.IRBuilder()
+
     with ib.function([x, y], "func"):
         with ib.dataflow() as df:
             lv0 = ib.emit(rx.op.add(x, y))
@@ -85,7 +106,12 @@ def test_function_multi_blocks():
             assert lv0.name_hint == "lv0"
             gv2 = ib.emit_output(gv1)
         ib.emit_output(gv2)
+
     func = ib.get()
+    assert gv2.shape[0] == m
+    assert gv2.shape[1] == n
+    assert gv2.checked_type.rank == 2
+    assert gv2.checked_type.dtype == "float16"
     assert func.params[0] == x
     assert func.params[1] == y
     assert func.name.name_hint == "func"
@@ -96,7 +122,40 @@ def test_function_multi_blocks():
     assert len(func.body.blocks[2].bindings) == 2
 
 
+def test_binary_shape_deduction():
+    m = tir.Var("m", "int32")
+    n = tir.Var("n", "int32")
+    k = tir.Var("k", "int32")
+    dtype0 = rx.DynTensorType(rank=2, dtype="float16")
+    dtype1 = rx.DynTensorType(rank=1, dtype="float16")
+    x = rx.Var("x", [m, 1], dtype0)
+    y = rx.Var("y", [n], dtype1)
+    z = rx.Var("z", [5], dtype0)
+    w = rx.Var("w", [k], dtype1)
+    ib = rx.IRBuilder()
+
+    with ib.function([x, y, z, w]):
+        with ib.dataflow() as df:
+            lv0 = ib.emit(rx.op.add(x, y))
+            assert lv0.shape[0] == m
+            assert lv0.shape[1] == n
+
+            lv1 = ib.emit(rx.op.multiply(x, z))
+            assert lv1.shape[0] == m
+            assert lv1.shape[1] == 5
+
+            lv2 = ib.emit(rx.op.multiply(z, w))
+            assert isinstance(lv2.shape, tvm.relay.Call)
+
+            lv3 = ib.emit(rx.op.multiply(y, w))
+            assert isinstance(lv3.shape, tvm.relay.Call)
+            gv0 = ib.emit_output(lv3)
+        ib.emit_output(gv0)
+        assert isinstance(gv0.shape, tvm.relay.Call)
+
+
 if __name__ == "__main__":
     test_dataflow_block()
     test_function_single_block()
     test_function_multi_blocks()
+    test_binary_shape_deduction()
