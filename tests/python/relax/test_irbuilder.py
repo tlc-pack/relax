5dbbc93f27113828249a16c5a756d1fd7e81021c
Yuchen Jin <yuchenj@cs.washington.edu>
2021-09-27 15:58:59 -0700

    Relax IRVisitor/IRMuator (#10)
    * ExprVisitor/ExprMutator for relax nodes.
    
    * Update Visitor & Mutator.
    
    * Update Mutator.
    
    * DataflowMutator interface.
    
    * EwiseFMARewriter.
    
    * Update fma rewrite and add test.
    
    * Update test.
    
    * Fix dataflow block dispatching.
    
    * Construct new dataflow block with IRBuilder.
    
    * VisitBinding return void and mutate internal IRBuilder.
    
    * Simplify.
    
    * Update emit dataflow output.
    
    * Explicit memeory allocation rewrite.
    
    * LazyIRBuilder.
    
    * Update ExplicitMemMutator.
    
    * Overload IRBuilder::Emit to have 3 styles.
    
    * Update IRBuilder/IRMutator interfaces and passes.
    
    * Add MatchShape binding to IRBuilder.
    
    * Improve IRMutator interface; add Normalize and CanProveShapeEqual to IRBuilder
    
    * Update EmitMatchShape.
    
    Co-authored-by: ZihengJiang <ziheng@apache.org>

--- tests/python/relax/test_irbuilder.py
+++ tests/python/relax/test_irbuilder.py
@@ -17,6 +17,7 @@
 
 import tvm
 from tvm import tir
+from tvm import relay
 from tvm import relax as rx
 
 
@@ -40,17 +41,23 @@ def test_dataflow_block():
 
         lv1 = ib.emit(rx.op.multiply(lv0, y))
         assert lv1.name_hint == "lv1"
-        gv0 = ib.emit_output(lv1)
+        
+        b0 = ib.match_shape(x, [m, n])
 
+        gv0 = ib.emit_output(lv1)
         assert gv0.name_hint == "gv0"
         assert gv0.shape[0] == m
         assert gv0.shape[1] == n
         assert gv0.checked_type.rank == 2
         assert gv0.checked_type.dtype == "float16"
+        assert isinstance(gv0, rx.Var)
 
     blocks = ib.get_blocks()
     assert len(blocks) == 1
-    assert len(blocks[-1].bindings) == 3
+    assert len(blocks[-1].bindings) == 4
+    for i in [0, 1, 3]:
+        assert isinstance(blocks[-1].bindings[i], rx.VarBinding)   
+    assert isinstance(blocks[-1].bindings[2], rx.MatchShape)
 
 
 def test_function_single_block():
@@ -122,7 +129,7 @@ def test_function_multi_blocks():
     assert len(func.body.blocks[2].bindings) == 2
 
 
-def test_binary_shape_deduction():
+def test_binary_shape_type_deduction():
     m = tir.Var("m", "int32")
     n = tir.Var("n", "int32")
     k = tir.Var("k", "int32")
@@ -130,7 +137,7 @@ def test_binary_shape_deduction():
     dtype1 = rx.DynTensorType(rank=1, dtype="float16")
     x = rx.Var("x", [m, 1], dtype0)
     y = rx.Var("y", [n], dtype1)
-    z = rx.Var("z", [5], dtype0)
+    z = rx.Var("z", [5], dtype1)
     w = rx.Var("w", [k], dtype1)
     ib = rx.IRBuilder()
 
@@ -139,23 +146,103 @@ def test_binary_shape_deduction():
             lv0 = ib.emit(rx.op.add(x, y))
             assert lv0.shape[0] == m
             assert lv0.shape[1] == n
+            assert isinstance(lv0.checked_type, rx.DynTensorType)
+            assert lv0.checked_type.rank == 2
+            assert lv0.checked_type.dtype == "float16"
 
             lv1 = ib.emit(rx.op.multiply(x, z))
             assert lv1.shape[0] == m
             assert lv1.shape[1] == 5
+            assert isinstance(lv1.checked_type, rx.DynTensorType)
+            assert lv1.checked_type.rank == 2
+            assert lv1.checked_type.dtype == "float16"
 
             lv2 = ib.emit(rx.op.multiply(z, w))
             assert isinstance(lv2.shape, tvm.relay.Call)
+            assert isinstance(lv2.checked_type, rx.DynTensorType)
+            assert lv2.checked_type.rank == 1
+            assert lv2.checked_type.dtype == "float16"
 
             lv3 = ib.emit(rx.op.multiply(y, w))
             assert isinstance(lv3.shape, tvm.relay.Call)
-            gv0 = ib.emit_output(lv3)
+            assert isinstance(lv3.checked_type, rx.DynTensorType)
+            assert lv3.checked_type.rank == 1
+            assert lv3.checked_type.dtype == "float16"
+
+            gv0 = ib.emit_output(lv3)            
+
         ib.emit_output(gv0)
         assert isinstance(gv0.shape, tvm.relay.Call)
+        assert isinstance(gv0.checked_type, rx.DynTensorType)
+        assert gv0.checked_type.rank == 1
+        assert gv0.checked_type.dtype == "float16"
+
+
+def test_emit_match_shape():
+    m = tir.Var("m", dtype="int32")
+    n = tir.Var("n", dtype="int32")
+    type_anno0 = rx.DynTensorType(-1, "float32")
+    x = rx.Var("tensor_value", type_annotation=type_anno0)
+    shape_anno = [16, 8]
+    y = rx.Var("shape_value", type_annotation=rx.ShapeType(), shape_annotation=shape_anno)
+    ib = rx.IRBuilder()
+
+    with ib.function([x, y]):
+        with ib.dataflow() as df:
+            # lv0: Tensor[(m, n), "float32"] =
+            #   match_shape(x: Tensor[_, "float32"], [m, n])
+            lv0 = ib.match_shape(x, [m, n])
+            assert isinstance(lv0, rx.DataflowVar)
+            assert lv0.shape[0] == m
+            assert lv0.shape[1] == n
+            assert lv0.checked_type.rank == 2
+            assert lv0.checked_type.dtype == "float32"
+
+            # lv1: Shape = match_shape(shape, [m, n])
+            lv1 = ib.match_shape(y, [m, n])
+            assert lv1.checked_type == rx.ShapeType()
+            gv0 = ib.emit_output(lv1)            
+
+        ib.emit_output(gv0)
+
+    block = ib.get_blocks()[-1]
+    b0, b1 = block.bindings[:2]
+    assert isinstance(b0, rx.MatchShape)
+    assert isinstance(b1, rx.MatchShape)
+
+    assert b0.value == x
+    assert b0.pattern[0] == m
+    assert b0.pattern[1] == n
+    assert b0.var == lv0
+
+    assert b1.value == y
+    assert b1.pattern[0] == m
+    assert b1.pattern[1] == n
+    assert b1.var == lv1
+
+
+def test_normalize():
+    m = tir.Var("m", "int32")
+    n = tir.Var("n", "int32")
+    dtype0 = rx.DynTensorType(rank=2, dtype="float16")
+    dtype1 = rx.DynTensorType(rank=1, dtype="float16")
+    x = rx.Var("x", [m, n], dtype0)
+    y = rx.Var("y", [n], dtype1)
+    ib = rx.IRBuilder()
+
+    add_call = rx.op.multiply(x, y)
+    assert isinstance(add_call.shape, relay.Call)
+
+    ib.normalize(add_call)
+    assert isinstance(add_call.shape, rx.ShapeExpr)
+    assert add_call.shape[0] == m
+    assert add_call.shape[1] == n
 
 
 if __name__ == "__main__":
     test_dataflow_block()
     test_function_single_block()
     test_function_multi_blocks()
-    test_binary_shape_deduction()
+    test_binary_shape_type_deduction()
+    test_emit_match_shape()
+    test_normalize()
