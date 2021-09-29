8af23f84c688ef1d31f533d88e93993e1ac3ffef
ZihengJiang <ziheng@apache.org>
2021-09-29 16:30:00 -0700

    Improve test.

--- src/relax/transform/shape_lower.cc
+++ src/relax/transform/shape_lower.cc
@@ -63,7 +63,7 @@ class ShapeLowerMutator : public ExprMutator {
       }
       // prepare mapping and heap var
       expr2slot_ = PrepareExpr2Slot(Downcast<Function>(p.second));
-      LOG(INFO) << "mapping: " << expr2slot_;
+      // LOG(INFO) << "mapping: " << expr2slot_;
       heap_size_ = IntImm(ShapeDType(), expr2slot_.size());
       DynTensorType heap_type(1, ShapeDType());
       shape_heap_ = Var("shape_heap", ShapeExpr({heap_size_}), heap_type);
