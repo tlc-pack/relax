b75b0ba4ca93f319709dd7f91de182482e24a001
Ziheng Jiang <ziheng@apache.org>
2021-10-22 14:56:42 -0700

    End2End Lowering (#23)
    * call_dps lowering.
    
    * Improve shape lowering.
    
    * Support alloc_storage for dynamic shape.
    
    * implementt ToNonDF to transform program to non-dataflow format.
    
    * Fix the mutator issue.
    
    * Update build api, an issue occurred.
    
    * vm tests can pass.
    
    * Support shape tuple in executable seriablization.
    
    * Fix for test.
    
    * Minor fixes.
    
    * Address comments.
    
    * Add mutate binding var back.
    
    * Visit binding var and fix tests.
    
    Co-authored-by: YuchenJin <yuchenj@cs.washington.edu>

--- tests/python/relax/test_transform.py
+++ tests/python/relax/test_transform.py
@@ -62,27 +62,65 @@ def test_fma_rewrite():
     assert type(func.body.blocks[0].bindings[1].var) == rx.Var
 
 
-def test_explicit_memory_rewrite():
-    m = tir.Var("m", "int32")
-    n = tir.Var("n", "int32")
-    shape_anno = [m, n]
-    type_anno = rx.DynTensorType(2, "float32")
-    x = rx.Var("x", shape_anno, type_anno)
-    ib = rx.BlockBuilder()
-    with ib.function(x):
-        with ib.dataflow() as df:
-            gv0 = ib.emit_output(rx.call_dps([m, n], rx.extern("test.op.identity"), [x]))
-        ib.emit_func_output(gv0)
-    expr = ib.get()
+def test_to_non_dataflow():
+    @rx.script
+    class TestToNoneDataflow:
+        def foo(x: Tensor[(m, n), "float32"]):
+            with relax.dataflow():
+                gv0 = relax.call_dps((m, n), "test.op.identity", (x,))
+                gv1 = relax.call_dps((m, n), "test.op.identity", (gv0,))
+                relax.output(gv1)
+            return gv1
+
+    mod = TestToNoneDataflow()
+
+    old_vars = [] 
+    def fvisit(e):
+        if isinstance(e, rx.Var):
+            nonlocal old_vars
+            old_vars.append(e)
+    rx.analysis.post_order_visit(mod["foo"], fvisit)
+    _, x, _, gv0, _, gv1 = old_vars
+
+    new_mod = rx.transform.to_non_dataflow(mod)
+
+    new_vars = []
+    def fvisit(e):
+        if isinstance(e, rx.Var):
+            nonlocal new_vars
+            new_vars.append(e)
+    rx.analysis.post_order_visit(new_mod["foo"], fvisit)
+
+    assert x == new_vars[1]
+    assert gv0 != new_vars[3]
+    assert isinstance(gv0, rx.DataflowVar)
+    assert not isinstance(new_vars[3], rx.DataflowVar)
+
+    assert isinstance(gv1, rx.Var)
+    assert isinstance(new_vars[5], rx.Var)
+    assert gv1 != new_vars[5]
+
+
+def test_call_dps_rewrite():
+    @rx.script
+    class TestCallDpsRewrite:
+        def foo(x: Tensor[(m, n), "float32"]):
+            gv0 = relax.call_dps((m, n), "test.op.identity", (x,))
+            return gv0
+
+    mod = TestCallDpsRewrite()
+    code = rx.parser.astext(mod)
 
     # before rewrite
-    v0 = expr.body.blocks[0].bindings[0].var
-    s0 = expr.body.blocks[0].bindings[0].value
+    v0 = mod["foo"].body.blocks[0].bindings[0].var
+    s0 = mod["foo"].body.blocks[0].bindings[0].value
     assert isinstance(s0, tvm.relay.Call)
     assert s0.op.name == "relax.call_dps"
 
     # after rewrite
-    func = rx.transform.explicit_memory_rewrite(expr)
+    new_mod = rx.transform.call_dps_rewrite(mod)
+    func = new_mod["foo"]
+    code = rx.parser.astext(new_mod)
 
     # the dataflow block has changed to binding block due to the rewriting
     block = func.body.blocks[0]
@@ -92,20 +130,41 @@ def test_explicit_memory_rewrite():
     assert isinstance(s1, tvm.relay.Call)
     assert s1.op.name == "relax.builtin.alloc_tensor"
     assert isinstance(s1.args[0], rx.ShapeExpr)
-    assert structural_equal(s1.args[0], rx.ShapeExpr(shape_anno))
+    assert structural_equal(s1.args[0], s0.args[0])
     s2 = block.bindings[1].value
     assert s2.op.global_symbol == "test.op.identity"
 
 
-@rx.script
-class Mod:
-    def foo(x: Tensor[_, "float32"]) -> Shape:
-        sh = relax.call_packed("vm.builtin.shape_of", x)
-        relax.match_shape(sh, (n, m))
-        return (n * 2, m * 3)
+def test_memory_lower():
+    @rx.script
+    class TestMemoryLower:
+        def foo(x: Tensor[(m, n), "float32"]):
+            alloc = relax.builtin.alloc_tensor((m, n))
+            _ = relax.call_packed("test.op.identity", (x,), alloc)
+            gv0 = alloc
+            return gv0
+    
+    mod = TestMemoryLower()
+
+    # after memory lowering
+    new_mod = rx.transform.memory_lower(mod)
+
+    assert isinstance(new_mod, tvm.IRModule)
+    assert isinstance(new_mod["foo"], tvm.relax.expr.Function)
+    code = rx.parser.astext(new_mod)
+    assert "vm.builtin.alloc_storage" in code
+    assert "vm.builtin.alloc_tensor" in code
+
 
 def test_shape_lowering():
-    mod = Mod()
+    @rx.script
+    class TestShapeLower:
+        def foo(x: Tensor[_, "float32"]) -> Shape:
+            sh = relax.call_packed("vm.builtin.shape_of", x)
+            relax.match_shape(sh, (n, m))
+            return (n * 2, m * 3)
+
+    mod = TestShapeLower()
     new_mod = rx.transform.shape_lower(mod)
     assert isinstance(new_mod, tvm.IRModule)
     assert isinstance(new_mod["shape_func"], tvm.tir.function.PrimFunc)
@@ -118,5 +177,7 @@ def test_shape_lowering():
 
 if __name__ == "__main__":
     test_fma_rewrite()
-    test_explicit_memory_rewrite()
+    test_to_non_dataflow()
+    test_call_dps_rewrite()
+    test_memory_lower()
     test_shape_lowering()
