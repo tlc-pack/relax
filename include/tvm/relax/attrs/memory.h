e498553fc6c9dde7d130c305ab184702fee132e9
Yuchen Jin <yuchenj@cs.washington.edu>
2021-11-04 11:05:42 -0700

    VM compiler refactor (#25)
    * Return Instruction::Arg for each CodeGenLLVM::VisitExpr_.
    
    * Change VMCompiler to be an Object from ModuleNode.
    
    * Introduce intrinsics and attrs.
    
    * Generic handling of attribute codegen.
    
    * Do to-non-dataflow transform in call_dps_rewrite.
    
    * Back to special attr handling.
    
    * Address comments.
    
    * Standalone to_non_dataflow pass; more tests.
    
    * Rename decode/make shape to store/load shape.
    
    * Update.
    
    * Fix namespace, add comments.
    
    * rebase
    
    * Rename files.
    
    * nit

--- include/tvm/relax/attrs/memory.h
+++ include/tvm/relax/attrs/memory.h
@@ -29,29 +29,31 @@
 namespace tvm {
 namespace relax {
 /*!
- * \brief Options for allocating storage.
+ * \brief Attributes for allocating storage.
  */
 struct AllocStorageAttrs : public tvm::AttrsNode<AllocStorageAttrs> {
-  DataType dtype;
-  int device_id;
   int device_type;
+  DataType dtype;
 
   TVM_DECLARE_ATTRS(AllocStorageAttrs, "relax.attrs.AllocStorageAttrs") {
+    TVM_ATTR_FIELD(device_type).describe("The device type on which to allocate memory.");
     TVM_ATTR_FIELD(dtype)
         .describe("The dtype of the tensor to allocate.")
         .set_default(DataType::Float(32, 1));
-    TVM_ATTR_FIELD(device_id).describe("The device id on which to allocate memory.");
-    TVM_ATTR_FIELD(device_type).describe("The device type on which to allocate memory.");
   }
 };
 
 /*!
- * \brief Options for allocating tensors.
+ * \brief Attributes for allocating tensors.
  */
 struct AllocTensorAttrs : public tvm::AttrsNode<AllocTensorAttrs> {
+  int offset;
   DataType dtype;
 
   TVM_DECLARE_ATTRS(AllocTensorAttrs, "relax.attrs.AllocTensorAttrs") {
+    TVM_ATTR_FIELD(offset)
+        .describe("Storage offset to allocate the tensor.")
+        .set_default(0);
     TVM_ATTR_FIELD(dtype)
         .describe("The dtype of the tensor to allocate.")
         .set_default(DataType::Float(32, 1));
