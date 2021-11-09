99401ac1bbe6b28a751125950b4d28b764fc846a
Yuchen Jin <yuchenj@cs.washington.edu>
2021-11-08 17:45:40 -0800

    Fix vm build. (#35)

--- src/relax/backend/vm/codegen_vm.h
+++ src/relax/backend/vm/codegen_vm.h
@@ -19,11 +19,11 @@
 
 /*!
  * \file src/relax/backend/vm/codegen_vm.h
- * \brief A compiler to compile an IRModule to VM executable.
+ * \brief A codegen to generate VM executable from an IRModule with relax functions.
  */
 
-#ifndef TVM_RELAX_BACKEND_VM_COMPILER_H_
-#define TVM_RELAX_BACKEND_VM_COMPILER_H_
+#ifndef TVM_RELAX_BACKEND_CODEGEN_VM_H_
+#define TVM_RELAX_BACKEND_CODEGEN_VM_H_
 
 #include <tvm/ir/module.h>
 #include <tvm/relax/vm/exec_builder.h>
@@ -40,37 +40,29 @@ using tvm::Target;
 using namespace tvm::runtime::relax_vm;
 using namespace tvm::runtime;
 
-class VMCompiler : public Object {
+class VMCodeGen : public Object {
  public:
   /*!
    * \brief Compile the functions in a Module.
-   * \param mod Input IRModule to be compiled.
+   * \param rx_mod Input IRModule that constains relax functions.
    */
-  void Compile(IRModule mod, Target target, Target target_host);
+  void CodeGen(IRModule rx_mod);
   /*!
    * \brief Get the compiled executable.
    * \return The compiled executable.
    */
   Executable GetExec();
-  /*!
-   * \brief Get the compiled library.
-   * \return The compiled lirary.
-   */
-  Module GetLib();
 
   static constexpr const uint32_t _type_index = TypeIndex::kDynamic;
-  static constexpr const char* _type_key = "relax.VMCompiler";
-  TVM_DECLARE_FINAL_OBJECT_INFO(ExecutableNode, Object);
+  static constexpr const char* _type_key = "relax.VMCodeGen";
 
  protected:
   /*! \brief Internal executable builder. */
   relax::ExecBuilder builder_;
-  /*! \brief Built library. */
-  runtime::Module lib_;
 };
 
 }  // namespace relax_vm
 }  // namespace relax
 }  // namespace tvm
 
-#endif  // TVM_RELAX_BACKEND_VM_COMPILER_H_
+#endif  // TVM_RELAX_BACKEND_CODEGEN_VM_H_
