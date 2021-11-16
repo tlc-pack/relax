1ced8f2652da0385f7499d2ce578885435ccdce7
Andrew Liu <andrewlliu@gmail.com>
2021-11-16 11:18:46 -0800

    run vm test_te

--- tests/python/relax/test_te.py
+++ tests/python/relax/test_te.py
@@ -28,7 +28,18 @@ for var in tir_mod:
 
 print(module.astext())
 
-# target = tvm.target.Target("llvm")
-# target_host = tvm.target.Target("llvm")
-# ex, lib = rx.vm.build(module, target, target_host)
-# vm = rx.VirtualMachine(ex, tvm.cpu(), mod=lib)
+import tvm.script
+print(tvm.script.relax.parser.astext(func))
+print(module.astext())
+
+target = tvm.target.Target("llvm")
+target_host = tvm.target.Target("llvm")
+ex, lib = rx.vm.build(module, target, target_host)
+vm = rx.VirtualMachine(ex, tvm.cpu(), mod=lib)
+import numpy as np
+shape = (128, 128)
+inp = tvm.nd.array(np.random.rand(*shape).astype(np.float32))
+inp2 = tvm.nd.array(np.random.rand(*shape).astype(np.float32))
+res = vm["rx_func"](inp, inp2)
+
+np.testing.assert_allclose(res.asnumpy(), inp.asnumpy() + inp2.asnumpy())
