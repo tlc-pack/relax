dbbf4f2763e61d165073fdc5d757081697ef64a5
Andrew Liu <andrewlliu@gmail.com>
2021-11-21 12:38:49 -0800

    spelling

--- src/relax/ir/emit_te.h
+++ src/relax/ir/emit_te.h
@@ -31,7 +31,7 @@ namespace tvm {
 namespace relax {
 
 /*!
- * \brief A placholder op that represents a relax expression.
+ * \brief A placeholder op that represents a relax expression.
  */
 class RXPlaceholderOpNode : public te::PlaceholderOpNode {
  public:
