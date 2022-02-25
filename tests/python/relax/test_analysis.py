f625691ca4e9fbe74f3fdc95952ae34fbcfc3e2f
Ziheng Jiang <ziheng@apache.org>
2022-02-25 12:18:08 -0800

    [TESTS] Enable Tests (#78)
    * Enable tests.
    
    * Updated.
    
    * Updated.
    
    * Updated.

--- tests/python/relax/test_analysis.py
+++ tests/python/relax/test_analysis.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import pytest
 import tvm
 from tvm import tir
 from tvm import relax as rx
@@ -66,45 +67,5 @@ def test_post_order_visit():
     assert names == ["relax.add", "relax.multiply"]
 
 
-# def test_lazy_irbuilder():
-#     m = tir.Var("m", "int32")
-#     n = tir.Var("n", "int32")
-#     dtype0 = rx.DynTensorType(rank=2, dtype="float16")
-#     dtype1 = rx.DynTensorType(rank=2, dtype="float16")
-#     x = rx.Var("x", [m, n], dtype0)
-#     y = rx.Var("y", [m, n], dtype1)
-#     ib = rx.BlockBuilder()
-
-#     # This program should not be rewritten by the fma_rewriter
-#     with ib.function([x, y]):
-#         with ib.dataflow() as df:
-#             lv0 = ib.emit(rx.op.multiply(x, y))
-#             lv1 = ib.emit(rx.op.multiply(lv0, y))
-#             gv0 = ib.emit_output(lv1)
-#         ib.emit_func_output(gv0)
-#     expr = ib.get()
-
-#     # before rewrite
-#     block0 = expr.body.blocks[0]
-#     v0 = expr.body.blocks[0].bindings[1].var
-#     s0 = expr.body.blocks[0].bindings[1].value
-#     assert isinstance(s0, tvm.relay.Call)
-#     assert s0.op.name == "relax.multiply"
-
-#     # after rewrite (the bindings and the dataflow block are reused)
-#     func = rx.transform.fma_rewrite(expr)
-
-#     block1 = func.body.blocks[0]
-#     v1 = func.body.blocks[0].bindings[1].var
-#     s1 = func.body.blocks[0].bindings[1].value
-
-#     # the dataflow block and vars are reused
-#     assert block0 == block1
-#     assert v1 == v0
-#     assert s1 == s0
-
-
 if __name__ == "__main__":
-    test_dispatch_var()
-    test_post_order_visit()
-    # test_lazy_irbuilder()
+    pytest.main([__file__])
