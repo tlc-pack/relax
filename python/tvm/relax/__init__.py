c1ccbb676f72d993e5dd0f0b8f5083e7186a6faa
Yuchen Jin <yuchenj@cs.washington.edu>
2022-03-17 18:51:24 -0700

    Change call_tir convention; Unify shape/type deduction rule (#94)
    * Change call_tir convention and fix shape/type deduction.
    
    * test
    
    * output shape as 3rd arg.
    
    * address comments.
    
    * lint

--- python/tvm/relax/__init__.py
+++ python/tvm/relax/__init__.py
@@ -57,6 +57,7 @@ Type = ty.Type
 ShapeType = ty.ShapeType
 DynTensorType = ty.DynTensorType
 DimType = ty.DimType
+TupleType = ty.TupleType
 
 # VM
 ExecBuilder = exec_builder.ExecBuilder
@@ -65,7 +66,7 @@ load_exec_from_file = vm.load_exec_from_file
 
 # Operator
 from .op.base import call_tir
-from .op.op_attrs import AllocStorageAttrs, AllocTensorAttrs
+from .op.op_attrs import VMAllocStorageAttrs, VMAllocTensorAttrs
 
 # IRBuilder
 BlockBuilder = block_builder.BlockBuilder
