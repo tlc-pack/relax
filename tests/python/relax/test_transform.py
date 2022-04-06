00f7f29cf59e03135936c98277bf1b35a5b5fa9b
Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
2022-04-06 11:59:33 -0700

    DataflowBlockPass (#114)
    * add DataflowBlockPass
    
    * update fma_rewrite
    
    * drop the skip function
    
    * update test_fma_rewrite with DataflowBlockPass
    
    * fix the format
    
    * fix name
    
    * rewrite test in tvm script
    
    * add non-dataflow Vars check
    
    * add fail testcases
    
    * module->IRModule
    
    * add docstring to DataflowBlockNode
    
    * remove unused pattern
    
    * Transform Pass->DataflowBlock Pass
    
    * rename global var to global scope var
    
    * remove print stmt
    
    * reformat tests
    
    * add docstring to DataflowBlockMutator
    
    * fix filename
    
    * minor fix

--- tests/python/relax/test_transform.py
+++ tests/python/relax/test_transform.py
@@ -29,44 +29,94 @@ from tvm.script import tir as T, relax as R
 
 
 def test_fma_rewrite():
-    m = tir.Var("m", "int32")
-    n = tir.Var("n", "int32")
-    type_anno0 = relax.DynTensorType(rank=2, dtype="float16")
-    type_anno1 = relax.DynTensorType(rank=2, dtype="float16")
-    x = relax.Var("x", [m, n], type_anno0)
-    y = relax.Var("y", [m, n], type_anno1)
-    ib = relax.BlockBuilder()
-    with ib.function("func", [x, y]):
-        with ib.dataflow() as df:
-            lv0 = ib.emit(relax.op.multiply(x, y))
-            gv0 = ib.emit_output(relax.op.add(lv0, y))
-        ib.emit_func_output(gv0)
-    mod = ib.get()
-    func = mod["func"]
+    @tvm.script.ir_module
+    class Before:
+        @R.function
+        def main(x: Tensor[(m, n), "float32"], y: Tensor[(m, n), "float32"]):
+            with relax.dataflow():
+                lv0 = relax.multiply(x, y)
+                gv0 = relax.add(lv0, y)
+                relax.output(gv0)
+            gv1 = relax.multiply(x, y)
+            gv2 = relax.add(gv1, y)
+            return (gv0, gv1, gv2)
 
-    # before rewrite
-    v0 = func.body.blocks[0].bindings[1].var
-    s0 = func.body.blocks[0].bindings[1].value
-    assert isinstance(s0, tvm.relay.Call)
-    assert s0.op.name == "relax.add"
-    assert structural_equal(v0.shape, relax.ShapeExpr([m, n]))
-    assert structural_equal(s0.shape, relax.ShapeExpr([m, n]))
-    assert structural_equal(gv0.shape, relax.ShapeExpr([m, n]))
+    @tvm.script.ir_module
+    class Expected:
+        @R.function
+        def main(x: Tensor[(m, n), "float32"], y: Tensor[(m, n), "float32"]):
+            with relax.dataflow():
+                lv0 = relax.multiply(x, y)
+                gv0 = relax.ewise_fma(x, y, y)
+                relax.output(gv0)
+            gv1 = relax.multiply(x, y)
+            gv2 = relax.add(gv1, y)
+            return (gv0, gv1, gv2)
 
-    # after rewrite
-    new_mod = relax.transform.FMARewrite()(mod)
-    new_func = new_mod["func"]
-    v1 = new_func.body.blocks[0].bindings[1].var
-    s1 = new_func.body.blocks[0].bindings[1].value
-    assert isinstance(s1, tvm.relay.Call)
-    assert s1.op.name == "relax.ewise_fma"
-    assert structural_equal(v1.shape, relax.ShapeExpr([m, n]))
-    assert structural_equal(s1.shape, relax.ShapeExpr([m, n]))
+    After = relax.transform.FMARewrite()(Before)
 
-    # The var binded to the fma call is reused because the shape
-    # and type of var are unchanged after rewriting
-    assert gv0 == v0
-    assert type(new_func.body.blocks[0].bindings[1].var) == relax.Var
+    assert_structural_equal(After, Expected)
+
+
+def test_dataflowpass_fail():
+    # raise error on rewriting/removing existing Global Vars inside the dataflow block.
+    with pytest.raises(tvm.TVMError):
+
+        @tvm.script.ir_module
+        class TestRemoveGlobalScopeVar:
+            @R.function
+            def main(x: Tensor[_, "float32"], y: Tensor[_, "float32"]):
+                with relax.dataflow():
+                    gv_remove = relax.add(x, y)
+                    gv1 = relax.add(x, y)
+                    relax.output(gv_remove, gv1)
+                return gv_remove, gv1
+
+        relax.transform.FailTestRewrite()(TestRemoveGlobalScopeVar)
+
+    with pytest.raises(tvm.TVMError):
+
+        @tvm.script.ir_module
+        class TestRewriteGlobalScopeVar:
+            @R.function
+            def main(x: Tensor[_, "float32"], y: Tensor[_, "float32"]):
+                with relax.dataflow():
+                    gv_rewrite = relax.add(x, y)
+                    gv1 = relax.add(x, y)
+                    relax.output(gv_rewrite, gv1)
+                return gv_rewrite, gv1
+
+        relax.transform.FailTestRewrite()(TestRewriteGlobalScopeVar)
+
+    # raise error on rewriting/removing existing Symbolic Vars inside the dataflow block
+    # check all Symbolic Vars defined in R.match_shape
+    with pytest.raises(tvm.TVMError):
+
+        @tvm.script.ir_module
+        class TestRewriteSymbolicVar:
+            @R.function
+            def main(x: Tensor[_, "float32"], y: Tensor[_, "float32"]):
+                with relax.dataflow():
+                    lv0 = R.match_shape(x, (m, n))
+                    gv0 = relax.add(lv0, y)
+                    relax.output(gv0)
+                return gv0
+
+        relax.transform.FailTestRewrite()(TestRewriteSymbolicVar)
+
+    with pytest.raises(tvm.TVMError):
+
+        @tvm.script.ir_module
+        class TestRemoveSymbolicVar:
+            @R.function
+            def main(x: Tensor[_, "float32"], y: Tensor[_, "float32"]):
+                with relax.dataflow():
+                    lv0 = R.match_shape(x, (m, n, d))
+                    gv0 = relax.add(lv0, y)
+                    relax.output(gv0)
+                return gv0
+
+        relax.transform.FailTestRewrite()(TestRemoveSymbolicVar)
 
 
 def test_visit_shape():
