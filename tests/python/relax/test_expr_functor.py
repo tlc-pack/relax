906c7bea291f280078bcdbd182854c59805e7a9e
Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
2022-04-22 08:05:13 -0700

    [Pass] Python pass decorator and ExprFunctor (#126)
    * Relax ExprFunctor in Python
    
    * fix the register bug
    
    * Expr_functor in relax
    
    * function/dataflowblock Pass in python
    
    * testcases
    
    * reformat
    
    * fix Tensor annotation()
    
    * add return type hint
    
    * type hint
    
    * new test
    
    * fix typo
    
    * remove memo

---
+++ tests/python/relax/test_expr_functor.py
@@ -0,0 +1,146 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+import numpy as np
+import pytest
+
+import tvm
+from tvm import relax, tir
+from tvm.relax import ExprFunctor, ExprMutator, ExprVisitor
+from tvm.ir.base import assert_structural_equal
+
+m, n = tir.Var("m", "int64"), tir.Var("n", "int64")
+type_anno1 = relax.DynTensorType(1, "float32")
+type_anno2 = relax.DynTensorType(2, "float32")
+x = relax.Var("x", [n], type_anno1)
+y = relax.Var("y", [m, n], type_anno2)
+
+
+def check_visit(expr):
+    with pytest.raises(NotImplementedError):
+        ef = ExprFunctor()
+        ef.visit(expr)
+
+    ev = ExprVisitor()
+    ev.visit(expr)
+
+    em = ExprMutator()
+    assert_structural_equal(em.visit(expr), expr)
+
+
+def test_constant():
+    check_visit(relax.const(1.0))
+
+
+def test_var():
+    check_visit(x)
+
+
+def test_dataflow_var():
+    lv = relax.DataflowVar("lv", [n], type_anno1)
+    check_visit(lv)
+
+
+def test_tuple():
+    t = relax.Tuple([x, y])
+    check_visit(t)
+
+
+def test_global_var():
+    gv = relax.GlobalVar("gv")
+    check_visit(gv)
+
+
+def test_seq_expr():
+    bindings = [relax.VarBinding(x, relax.const(1))]
+    blocks = [relax.BindingBlock(bindings)]
+    seq_expr = relax.SeqExpr(blocks, x)
+    check_visit(seq_expr)
+
+
+def test_shape_expr():
+    x = relax.ShapeExpr([m, n])
+    check_visit(x)
+
+
+def test_runtime_dep_shape():
+    runtime_dep_shape = relax.RuntimeDepShape()
+    check_visit(runtime_dep_shape)
+
+
+def test_call():
+    call_node = relax.op.add(x, y)
+    check_visit(call_node)
+
+
+def test_if():
+    if_node = relax.If(x, x, x)
+    check_visit(if_node)
+
+
+def test_tuple_getitem():
+    op = relax.TupleGetItem(relax.Tuple([x, y]), 0)
+    check_visit(op)
+
+
+def test_match_shape():
+    shape = relax.const([16, 8], "int32")
+    b0 = relax.MatchShape(shape, [m, n], y)
+    check_visit(b0)
+
+
+def test_var_binding():
+    val = relax.const(np.random.rand(24, 56))
+    b0 = relax.VarBinding(y, val)
+    check_visit(b0)
+
+
+def test_binding_block():
+    shape = relax.const([16, 8], "int32")
+    b0 = relax.MatchShape(shape, [m, n], y)
+    val = relax.const(np.random.rand(24, 56))
+    b1 = relax.VarBinding(y, val)
+
+    block0 = relax.BindingBlock([b0, b1])
+    check_visit(block0)
+
+
+def test_dataflow_block():
+    shape = relax.const([16, 8], "int32")
+    b0 = relax.MatchShape(shape, [m, n], y)
+    val = relax.const(np.random.rand(24, 56))
+    b1 = relax.VarBinding(y, val)
+
+    block0 = relax.DataflowBlock([b0, b1])
+    check_visit(block0)
+
+
+def test_function():
+    bindings = [relax.VarBinding(x, relax.const(1))]
+    blocks = [relax.BindingBlock(bindings)]
+    seq_expr = relax.SeqExpr(blocks, x)
+    ret_type = relax.DynTensorType(-1, "float32")
+    func = relax.Function([x], seq_expr, ret_type, relax.GlobalVar("func"))
+    check_visit(func)
+
+
+def test_extern_func():
+    func = relax.ExternFunc("f")
+    check_visit(func)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
---
+++ tests/python/relax/test_pass_manager.py
@@ -0,0 +1,240 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Unit tests for relax pass manager."""
+from __future__ import annotations  # must import to defer parsing of annotations
+import numpy as np
+import pytest
+import tvm
+from tvm import relax, tir
+from tvm.ir.base import assert_structural_equal
+from tvm.relax import ExprMutator
+from tvm.relax.expr import Call
+
+import tvm.script
+from tvm.script import tir as T, relax as R
+
+
+def check_equal(mod1, mod2):
+    mod1 = relax.transform.ToANF()(mod1)
+    mod2 = relax.transform.ToANF()(mod2)
+    assert_structural_equal(mod1, mod2)
+
+
+def test_function_class_pass():
+    @relax.transform.function_pass(opt_level=1)
+    class TestReplaceFunc:
+        """Simple test function to replace one argument to another."""
+
+        def __init__(self, new_func):
+            self.new_func = new_func
+
+        def transform_function(self, func, mod, ctx):
+            return self.new_func
+
+    @tvm.script.ir_module
+    class Before:
+        @R.function
+        def f1(x: Tensor((m, n), "float32")):
+            return x
+
+    @tvm.script.ir_module
+    class Expected:
+        @R.function
+        def f2(x: Tensor((m, n), "float32")):
+            gv0 = relax.add(x, x)
+            return gv0
+
+    fpass = TestReplaceFunc(Expected["f2"])
+    assert fpass.info.opt_level == 1
+    assert fpass.info.name == "TestReplaceFunc"
+    After = fpass(Before)
+    assert_structural_equal(After["f1"], Expected["f2"])
+
+
+# Swap Multiply and Add Ops
+class SwapMAVar(ExprMutator):
+    def __init__(self):
+        ExprMutator.__init__(self)
+
+    def visit_call(self, call: Call):
+        if call.op == tir.op.Op.get("relax.add"):
+            new_op = tir.op.Op.get("relax.multiply")
+        elif call.op == tir.op.Op.get("relax.multiply"):
+            new_op = tir.op.Op.get("relax.add")
+        else:
+            new_op = self.visit(call.op)
+        new_args = [self.visit(arg) for arg in call.args]
+        return Call(new_op, new_args, call.attrs, call.type_args, call.span)
+
+
+def test_function_pass():
+    @tvm.script.ir_module
+    class Before:
+        @R.function
+        def main(x: Tensor((m, n), "float32"), y: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.multiply(x, y)
+                gv0 = relax.add(lv0, y)
+                relax.output(gv0)
+            gv1 = relax.multiply(x, y)
+            gv2 = relax.add(gv1, y)
+            return (gv0, gv1, gv2)
+
+    @tvm.script.ir_module
+    class Expected:
+        @R.function
+        def main(x: Tensor((m, n), "float32"), y: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.add(x, y)
+                gv0 = relax.multiply(lv0, y)
+                relax.output(gv0)
+            gv1 = relax.add(x, y)
+            gv2 = relax.multiply(gv1, y)
+            return (gv0, gv1, gv2)
+
+    pass_name = "function_pass_test"
+    opt_level = 0
+
+    # create FunctionPass with the function_pass decorator
+    @relax.transform.function_pass(opt_level=opt_level, name=pass_name)
+    def decorator_transform(expr, mod, ctx):
+        return SwapMAVar().visit(expr)
+
+    # check the transform info
+    assert isinstance(decorator_transform, relax.transform.FunctionPass)
+    assert decorator_transform.info.name == pass_name
+    assert decorator_transform.info.opt_level == opt_level
+    # run the transform
+    After = decorator_transform(Before)
+    check_equal(After, Expected)
+
+    # create FunctionPass directly with the function_pass init call
+    def direct_transform(expr, mod, ctx):
+        return SwapMAVar().visit(expr)
+
+    direct_transform = relax.transform.function_pass(direct_transform, opt_level=opt_level)
+    assert isinstance(direct_transform, relax.transform.FunctionPass)
+    assert direct_transform.info.name == "direct_transform"
+    assert direct_transform.info.opt_level == opt_level
+    # run the transform
+    After = direct_transform(Before)
+    check_equal(After, Expected)
+
+
+def test_dataflowblock_class_pass():
+    @relax.transform.dataflowblock_pass(opt_level=1)
+    class TestReplaceBinding:
+        """Simple test function to replace the first VarBinding to another."""
+
+        def __init__(self):
+            # create a new VarBinding
+            m, n = tir.Var("m", "int64"), tir.Var("n", "int64")
+            type_anno = relax.DynTensorType(2, "float32")
+            lv0 = relax.Var("lv1", [m, n], type_anno)
+            val = relax.const(np.random.rand(24, 56))
+            self.new_binding = relax.VarBinding(lv0, val)
+
+        def transform_dataflowblock(self, block, mod, ctx):
+            bindings = block.bindings
+            new_bindings = [self.new_binding, bindings[1]]
+            new_block = relax.expr.DataflowBlock(new_bindings, block.span)
+            return new_block
+
+    @tvm.script.ir_module
+    class Mod1:
+        @R.function
+        def f1(x: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.multiply(x, x)
+                gv0 = relax.add(x, x)
+                relax.output(gv0)
+            return gv0
+
+    @tvm.script.ir_module
+    class Mod2:
+        @R.function
+        def f2(x: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.add(x, x)
+                gv0 = relax.add(x, x)
+                relax.output(gv0)
+            return gv0
+
+    block_pass = TestReplaceBinding()
+    assert block_pass.info.opt_level == 1
+    assert block_pass.info.name == "TestReplaceBinding"
+    updated_mod1 = block_pass(Mod1)
+    updated_mod2 = block_pass(Mod2)
+    assert_structural_equal(updated_mod1["f1"], updated_mod2["f2"])
+
+
+def test_dataflowblock_pass():
+    @tvm.script.ir_module
+    class Before:
+        @R.function
+        def main(x: Tensor((m, n), "float32"), y: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.multiply(x, y)
+                gv0 = relax.add(lv0, y)
+                relax.output(gv0)
+            gv1 = relax.multiply(x, y)
+            gv2 = relax.add(gv1, y)
+            return (gv0, gv1, gv2)
+
+    @tvm.script.ir_module
+    class Expected:
+        @R.function
+        def main(x: Tensor((m, n), "float32"), y: Tensor((m, n), "float32")):
+            with relax.dataflow():
+                lv0 = relax.add(x, y)
+                gv0 = relax.multiply(lv0, y)
+                relax.output(gv0)
+            gv1 = relax.multiply(x, y)
+            gv2 = relax.add(gv1, y)
+            return (gv0, gv1, gv2)
+
+    pass_name = "dataflow_pass_test"
+    opt_level = 0
+
+    # create DataflowBlockPass with the dataflowblock_pass decorator
+    @relax.transform.dataflowblock_pass(opt_level=opt_level, name=pass_name)
+    def decorator_transform(expr, mod, ctx):
+        return SwapMAVar().visit(expr)
+
+    # check the transform info
+    assert isinstance(decorator_transform, relax.transform.DataflowBlockPass)
+    assert decorator_transform.info.name == pass_name
+    assert decorator_transform.info.opt_level == opt_level
+    # run the transform
+    After = decorator_transform(Before)
+    check_equal(After, Expected)
+
+    # create DataflowBlockPass directly with the dataflowblock_pass init call
+    def direct_transform(expr, mod, ctx):
+        return SwapMAVar().visit(expr)
+
+    direct_transform = relax.transform.dataflowblock_pass(direct_transform, opt_level=opt_level)
+    assert isinstance(direct_transform, relax.transform.DataflowBlockPass)
+    assert direct_transform.info.name == "direct_transform"
+    assert direct_transform.info.opt_level == opt_level
+    # run the transform
+    After = direct_transform(Before)
+    check_equal(After, Expected)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
