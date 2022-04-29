d7007b16a8723604fa9970ace8e3acfdc8479192
Prakalp Srivastava <prakalp@octoml.ai>
2022-04-29 05:37:16 -0700

    Add Unique operator in Relax.
    This adds the functionality to register a packed function implementation of
    any operator using `FCallPacked` attribute. The relax operator would be
    lowered to a call to the registered packed function during codegen.
    For example, in this change relax.unique is lowered to
    `relax.run.unique` packed function which uses torch.unique under the
    hood.

---
+++ src/relax/op/tensor/unary.cc
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file unary.cc
+ * \brief unary operators.
+ */
+
+#include "unary.h"
+
+namespace tvm {
+namespace relax {
+
+TVM_REGISTER_NODE_TYPE(UniqueAttrs);
+
+RELAY_REGISTER_OP("relax.unique")
+    .describe(
+        "This operation returns the unique elements and the new index of each item in a given "
+        "tensor.")
+    .set_num_inputs(1)
+    .add_argument("data", "Tensor", "The input tensor")
+    .set_attrs_type<UniqueAttrs>()
+    .set_attr<FInferShape>("FInferShape", InferShapeUnique)
+    .set_attr<FInferType>("FInferType", InferTypeUnique)
+    .set_attr<FCallPacked>("FCallPacked", "relax.run.unique");
+
+Expr MakeUnique(Expr data, bool sorted, bool return_inverse, bool return_counts, int dim) {
+  auto attrs = make_object<UniqueAttrs>();
+  attrs->sorted = sorted;
+  attrs->return_inverse = return_inverse;
+  attrs->return_counts = return_counts;
+  attrs->dim = dim;
+  static const Op& op = Op::Get("unique");
+  return Call(op, {data}, Attrs(attrs));
+}
+
+TVM_REGISTER_GLOBAL("relax.op.unique").set_body_typed(MakeUnique);
+
+}  // namespace relax
+}  // namespace tvm
