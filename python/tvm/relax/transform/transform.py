21a30c11b205246a57d85a875b2b58a087e19294
Yuchen Jin <yuchenj@cs.washington.edu>
2022-04-30 18:45:14 -0700

    [BlockBuilder] Sub function call shape deduction: constant shape case. (#137)

--- python/tvm/relax/transform/transform.py
+++ python/tvm/relax/transform/transform.py
@@ -49,14 +49,25 @@ def FailTestRewrite() -> tvm.ir.transform.Pass:
     return _ffi_api.FailTestRewrite()
 
 
-def FMARewrite() -> tvm.ir.transform.Pass:
+def RewriteFMA() -> tvm.ir.transform.Pass:
     """Perform fused multiply add rewriting in dataflow blocks.
 
     Returns
     -------
     ret: tvm.ir.transform.Pass
     """
-    return _ffi_api.FMARewrite()
+    return _ffi_api.RewriteFMA()
+
+
+def FuseFMA() -> tvm.ir.transform.Pass:
+    """Perform fused multiply add rewriting, generate a subgraph(sub function),
+    and call into the sub function in the main function.
+
+    Returns
+    -------
+    ret: tvm.ir.transform.Pass
+    """
+    return _ffi_api.FuseFMA()
 
 
 def ToNonDataflow() -> tvm.ir.transform.Pass:
@@ -127,6 +138,7 @@ def MetaScheduleApplyHistoryBest(
     target: Target,
 ) -> tvm.ir.transform.Pass:
     """Apply the best schedule from tuning database.
+
     Parameters
     ----------
     database : metaschedule tuning database
@@ -160,7 +172,7 @@ def BindParams(func_name: str, params: Dict[str, tvm.runtime.NDArray]) -> tvm.ir
 
 
 def FoldConstant() -> tvm.ir.transform.Pass:
-    """Fold constant expressions
+    """Fold constant expressions.
 
     Returns
     -------
@@ -170,7 +182,7 @@ def FoldConstant() -> tvm.ir.transform.Pass:
 
 
 def _wrap_class_function_pass(pass_cls, pass_info):
-    """Wrap a python class as function pass"""
+    """Wrap a python class as function pass."""
 
     class PyFunctionPass(FunctionPass):
         """Internal wrapper class to create a class instance."""
