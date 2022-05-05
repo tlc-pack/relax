1ed1b5883f899070ca50c527d149f13e98962012
Prakalp Srivastava <prakalp@octoml.ai>
2022-05-04 20:52:16 -0700

    Add `relax.unique` operator in Relax. (#135)
    * Add Unique operator in Relax.
    
    This adds the functionality to register a packed function implementation of
    any operator using `FCallPacked` attribute. The relax operator would be
    lowered to a call to the registered packed function during codegen.
    For example, in this change relax.unique is lowered to
    `relax.run.unique` packed function which uses torch.unique under the
    hood.
    
    * Add support for integer constants in Relax VM.
    
    This adds serialization, deserialization, and print support for
    integer constants.

--- python/tvm/relax/op/tensor.py
+++ python/tvm/relax/op/tensor.py
@@ -14,15 +14,42 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 """Basic tensor operations."""
+import numpy as np
+import tvm
+
 from . import _ffi_api
 from ..expr import Expr
 
 
-def add(lhs: Expr,
-        rhs: Expr) -> Expr:
+def add(lhs: Expr, rhs: Expr) -> Expr:
     return _ffi_api.add(lhs, rhs)
 
 
-def multiply(lhs: Expr,
-             rhs: Expr) -> Expr:
+def multiply(lhs: Expr, rhs: Expr) -> Expr:
     return _ffi_api.multiply(lhs, rhs)
+
+
+@tvm.register_func("relax.run.unique")
+def unique(
+    a: tvm.nd.array,
+    sort: int,
+    return_inverse: int,
+    return_counts: int,
+    dim: int,
+) -> tvm.nd.array:
+    """Returns the unique elements of the input tensor.
+
+    Uses numpy.unique to compute unique elements.
+    """
+    # TODO(prakalp): add support for returning a tuple when return_inverse or return_counts is True
+    if bool(return_inverse) or bool(return_counts):
+        raise NotImplementedError("missing support return_inverse or return_counts set to true")
+    if dim < 0:
+        dim = None
+    a_numpy = a.numpy()
+    # TODO(prakalp): use torch.unique instead of numpy when torch is installed in ci.
+    output_sorted_numpy, indices = np.unique(a_numpy, return_index=True)
+    if sort:
+        return tvm.nd.array(output_sorted_numpy)
+    output_numpy = [a_numpy.flatten()[index] for index in sorted(indices, reverse=True)]
+    return tvm.nd.array(output_numpy)
