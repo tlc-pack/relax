1f9fba402a9cb8d84db979a3c878eaf1056eec7d
LeshengJin <leshenj15@gmail.com>
2022-06-15 20:18:42 -0700

    add header

--- python/tvm/relax/ir/instrument.py
+++ python/tvm/relax/ir/instrument.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=no-else-return, invalid-name
 """Common relax pass instrumentation across IR variants."""
 import tvm
 from tvm import relax
