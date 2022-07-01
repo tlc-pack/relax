e4f62a775970b7bd86ac0f3f02a3fcaee5d25e7c
Yuchen Jin <yuchenj@cs.washington.edu>
2022-07-01 09:31:30 -0700

    [Refactor] Generic dispatching for `IsBaseOf`; Simplify Type/Expr initializations; `relax` -> `R` in printer; Disallow local function in VMCodegen (#171)
    - Generic dispatching for `IsBaseOf`: `IsBaseOf` uses a bunch of if-else to check if the subtype relation between the base type and derived type, now it's changed to use a generic TypeFunctor to dispatch on the base class to do the check.
    - Simplify Type/Expr initializations: We had to write `RuntimeDepShape(Span()`), `ObjectType(Span())` to initialize several Types and Exprs, this is due to the `TVM_DEFINE_OBJECT_REF_METHODS` macro that sets the constructor with `= default`. By changing to use `TVM_DEFINE_NOTNULLABLE_OBJECT_REF_METHODS`, we can now just write `RuntimeDepShape()` without specifying an empty span.
    - `relax` -> `R` in printer: Change to print `R` rather than `relax` in TVMScript as the default behavior. This is consistent with our test cases and TIR convention: using `T` as shorthand.
    - Disallow generating code for local function in VMCodegen: these local functions should have been lifted in the lambda lifting pass before codegen.

--- tests/python/relax/test_type.py
+++ tests/python/relax/test_type.py
@@ -36,7 +36,9 @@ def test_dyn_tensor_type():
 
 
 def test_subtype():
-    # DynTensorType
+    # check the subtype relation for DynTensorType
+    # e.g., DynTensorType(ndim=3, dtype="float32") is a subtype of DynTensorType(ndim=-1, dtype="float32")
+    # and DynTensorType(ndim=-1, "float32") is a subtype of DynTensorType(ndim=-1, dtype=None)
     t0 = rx.DynTensorType(-1, None)
     t1 = rx.DynTensorType(3, None)
     t2 = rx.DynTensorType(3, "int32")
@@ -49,22 +51,98 @@ def test_subtype():
     assert is_base_of(t1, t2)
     assert is_base_of(t1, t3)
     assert is_base_of(t4, t3)
+    # a type is subtype of itself
+    assert is_base_of(t2, t2)
+    assert is_base_of(t3, t3)
     assert is_base_of(t2, t3) == False
     assert is_base_of(t3, t2) == False
 
-    # ShapeType
+    # check the subtype relation for ShapeType
     t5 = rx.ShapeType()
     t6 = rx.ShapeType()
+    assert is_base_of(t5, t5)
     assert is_base_of(t5, t6)
     assert is_base_of(t5, t0) == False
 
-    # TupleType
+    # check the subtype relation for TupleType by checking if each field
+    # of the base TupleType is subtype of the field of the derived TupleType
+    # e.g., TupleType([DynTensorType(ndim=3, dtype="float32"), ShapeType()])
+    # is a subtype of TupleType([DynTensorType(ndim=-1, dtype="float32"), ShapeType()])
     t7 = rx.TupleType([t0, t1, t5])
     t8 = rx.TupleType([t1, t1, t5])
     t9 = rx.TupleType([t1, t3, t5])
+    t10 = rx.TupleType([t5, t3, t1])
+    t11 = rx.TupleType([t1, t3])
     assert is_base_of(t7, t8)
     assert is_base_of(t7, t9)
-    assert is_base_of(t7, t9)
+    assert is_base_of(t8, t9)
+    assert is_base_of(t8, t8)
+    assert is_base_of(t9, t7) == False
+    assert is_base_of(t7, t10) == False
+    assert is_base_of(t11, t7) == False
+    assert is_base_of(t7, t11) == False
+
+    # check the subtype relation for FunctionType by checking the subtype relations of arg_types and ret_type
+    # e.g., FuncType([DynTensorType(ndim=3, dtype="float32")], DynTensorType(ndim=2, dtype="float32"))
+    # is a subtype of FuncType([DynTensorType(ndim=-1, dtype=None)], DynTensorType(ndim=-1, dtype="float32"))
+    t12 = rx.FuncType([t7], t0)
+    t13 = rx.FuncType([t7], t1)
+    t14 = rx.FuncType([t8], t0)
+    t15 = rx.FuncType([t8], t1)
+    t16 = rx.FuncType([t7, t0], t1)
+    t17 = rx.FuncType([t7, t4], t1)
+    assert is_base_of(t12, t13)
+    assert is_base_of(t12, t14)
+    assert is_base_of(t12, t15)
+    assert is_base_of(t12, t12)
+    assert is_base_of(t13, t14) == False
+    assert is_base_of(t13, t15)
+    assert is_base_of(t14, t15)
+    assert is_base_of(t16, t17)
+    assert is_base_of(t16, t16)
+    assert is_base_of(t12, t16) == False
+    assert is_base_of(t13, t16) == False
+
+    # check the subtype relation for ObjectType
+    # ObjectType is the base type of every type in Relax
+    t18 = rx.ObjectType()
+    assert is_base_of(t18, t0)
+    assert is_base_of(t18, t5)
+    assert is_base_of(t18, t7)
+    assert is_base_of(t18, t12)
+    assert is_base_of(t18, t18)
+    assert is_base_of(t0, t18) == False
+    assert is_base_of(t5, t18) == False
+    assert is_base_of(t7, t18) == False
+    assert is_base_of(t12, t18) == False
+
+    # more complicated cases
+    # TupleType with all possible types as fields
+    t19 = rx.TupleType([t7, t0, t5, t12, t18])
+    t20 = rx.TupleType([t8, t1, t5, t15, t18])
+    t21 = rx.TupleType([t18, t18, t18, t18, t18])
+    assert is_base_of(t19, t20)
+    assert is_base_of(t21, t19)
+    assert is_base_of(t21, t20)
+    assert is_base_of(t20, t19) == False
+    assert is_base_of(t18, t20)
+    # FuncType with all possible types as arg_types and ret_type
+    t22 = rx.FuncType([t7, t0, t5, t12, t18], t0)
+    t23 = rx.FuncType([t8, t1, t5, t15, t18], t1)
+    t24 = rx.FuncType([t7], t0)
+    t25 = rx.FuncType([t18, t18, t18, t18, t18], t18)
+    t26 = rx.FuncType([t18], t18)
+    t27 = rx.FuncType([t7, t0, t5, t12, t18], t19)
+    t28 = rx.FuncType([t7, t0, t5, t12, t18], t20)
+    assert is_base_of(t22, t23)
+    assert is_base_of(t25, t23)
+    assert is_base_of(t18, t23)
+    assert is_base_of(t18, t22)
+    assert is_base_of(t27, t28)
+    assert is_base_of(t24, t22) == False
+    assert is_base_of(t24, t23) == False
+    assert is_base_of(t26, t23) == False
+    assert is_base_of(t28, t27) == False
 
 
 if __name__ == "__main__":
