f3cc1233858d3f116ef15af2c69f0393b4919457
Jiawei Liu <jaway.liu@gmail.com>
2022-07-26 20:06:25 -0500

    [UX] Highlight TVMScript with Pygments (#185)

--- python/tvm/tir/function.py
+++ python/tvm/tir/function.py
@@ -199,6 +199,7 @@ class PrimFunc(BaseFunc):
     def show(self, style: Optional[str] = None) -> None:
         """
         A sugar for print highlighted TVM script.
+
         Parameters
         ----------
         style : str, optional
