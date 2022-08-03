ca6cf73687ad0f55b1cbe26d7efed26478500f50
Steven Lyubomirsky <slyubomirsky@octoml.ai>
2022-08-03 17:53:53 -0400

    Actually use the format string

--- python/tvm/relax/op/base.py
+++ python/tvm/relax/op/base.py
@@ -139,7 +139,7 @@ def relax_print(
     """
     Just prints whatever value is passed to it. Used for Relax's print op.
     """
-    if format == "":
+    if format_str == "":
         print(val)
     else:
         print(format_str.format(val))
