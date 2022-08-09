232efff831b320b2a0d8df628a2dc5551d3a0e85
Steven S. Lyubomirsky <slyubomirsky@octoml.ai>
2022-08-09 17:07:52 -0400

    [Op][Debugging] Add a print operator (#201)
    * Attempt at adding a print operator
    
    * Fix the registration
    
    * Actually use the format string
    
    * Improve test
    
    * Fix comment placement
    
    * Improve the docstring for relax_print
    
    * Handle tuples too
    
    * Formatting :(
    
    * Correct commit message
    
    * Match attr name across Python and C++
    
    * Make print variadic

--- python/tvm/relax/op/base.py
+++ python/tvm/relax/op/base.py
@@ -16,6 +16,7 @@
 """The base Relax operators."""
 from typing import Union, List, Optional
 
+import tvm
 from tvm.runtime.object import Object
 
 from . import _ffi_api
@@ -128,3 +129,56 @@ def invoke_closure(
         args = Tuple(args)
 
     return _ffi_api.invoke_closure(closure, args)
+
+
+@tvm.register_func("relax.run.print")
+def relax_print(*args: List[any]) -> None:
+    """
+    Takes a list of values to print, formats with the given format string.
+    If the format string is empty, simply prints.
+
+    Since this function is called as a PackedFunc from the generated code,
+    we cannot have it be variadic _and_ have an optional format string attribute
+    except by taking in all the arguments as a single list. The last argument
+    should be a format string.
+
+    Call from TVM script like this:
+    `relax.print(value1, value2, ..., valueN, format=format_str)`
+    or
+    `relax.print(value1, value2, ..., valueN) # format_str defaults to ""`
+
+    Parameters
+    ----------
+    vals: List[Object]
+        The values to print.
+
+    format_str: str
+        The last argument is a Python-style format string for printing the value
+    """
+
+    # there is no way to have a keyword arg to a packed function,
+    # so the format string is always the last argument
+    format_str = args[-1]
+    if not isinstance(format_str, str):
+        raise ValueError("No valid format string given.")
+
+    def render(val: tvm.Object) -> str:
+        if isinstance(val, tvm.runtime.ndarray.NDArray):
+            return str(val)
+        # no pretty-printer by default, so if we don't handle this,
+        # then we can't look inside tuples
+        if isinstance(val, tvm.runtime.container.ADT):
+            # the fields array of an ADT cannot be directly accessed in Python
+            # so we have to get the length and index into the fields separately
+            fields = ", ".join([render(val[i]) for i in range(len(val))])
+            # special case: tag = 0 is a tuple
+            if val.tag == 0:
+                return f"({fields})"
+            return f"ADT(tag={val.tag}, fields=[{fields}])"
+        return str(val)
+
+    val_strs = map(render, args[:-1])
+    if format_str == "":
+        print(*val_strs)
+    else:
+        print(format_str.format(*val_strs))
