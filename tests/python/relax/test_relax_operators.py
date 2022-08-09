29c83292ee01ef45b455c919ae13c3eab9ec4e66
Steven S. Lyubomirsky <slyubomirsky@octoml.ai>
2022-08-09 17:07:52 -0400

    [Op][Debugging] Add a print operator (#201)
    * Attempt at adding a print operator
    
    * Fix the registration
    
    * Actually use the format string
    
    * Improve test
    
    * Fix comment placement
    
    * Improve the docstring for relax_print
    
    * Handle tuples too
    
    * Formatting :(
    
    * Correct commit message
    
    * Match attr name across Python and C++
    
    * Make print variadic

--- tests/python/relax/test_relax_operators.py
+++ tests/python/relax/test_relax_operators.py
@@ -16,6 +16,8 @@
 # under the License.
 
 from __future__ import annotations  # must import to defer parsing of annotations
+import sys
+import tempfile
 import pytest
 import tvm
 from tvm import relax
@@ -34,23 +36,56 @@ class InputModule:
         return y, y_sorted
 
 
-def test_unique():
-
-    mod = InputModule
-    # TODO(prakalp): also add test for compiling and running on cuda device.
+def run_cpu(mod, func_name, *input):
     target = tvm.target.Target("llvm")
     ex = relax.vm.build(mod, target)
     vm = relax.VirtualMachine(ex, tvm.cpu())
+    return vm[func_name](*input)
 
+
+def test_unique():
+
+    # TODO(prakalp): also add test for compiling and running on cuda device.
     data_numpy = np.random.randint(0, 16, (16, 16))
     data = tvm.nd.array(data_numpy)
-    result, result_sorted = vm["foo"](data)
+    result, result_sorted = run_cpu(InputModule, "foo", data)
 
     expected_output_sorted, indices = np.unique(data_numpy, return_index=True)
     expected_output = [data_numpy.flatten()[index] for index in sorted(indices, reverse=True)]
 
     np.testing.assert_array_equal(expected_output_sorted, result_sorted.numpy())
     np.testing.assert_array_equal(expected_output, result.numpy())
+
+
+@tvm.script.ir_module
+class PrintTest:
+    @R.function
+    def foo(x: Tensor((), "int32")):
+        # results have to be bound, but we don't use them
+        # TODO: We should allow calls whose results are not bound for side effects;
+        #       it would be easy syntactic sugar to add.
+        p1 = relax.print(x)
+        p2 = relax.print(x, format="Number: {}")
+        t = (x, x)
+        p3 = relax.print(t, format="Tuple: {}")
+        p4 = relax.print(x, t)
+        p5 = relax.print(x, x, format="Custom print: {} {}")
+        p6 = relax.print(x, t, format="Another print: {} {}")
+        return x
+
+
+def test_print():
+    try:
+        stdout = sys.stdout
+        with tempfile.TemporaryFile(mode="w+") as test_out:
+            sys.stdout = test_out
+            run_cpu(PrintTest, "foo", tvm.nd.array(1))
+            test_out.seek(0)
+            printed_text = str(test_out.read())
+            expected = "1\nNumber: 1\nTuple: (1, 1)\n1 (1, 1)\nCustom print: 1 1\nAnother print: 1 (1, 1)\n"
+            assert printed_text == expected
+    finally:
+        sys.stdout = stdout
 
 
 if __name__ == "__main__":
