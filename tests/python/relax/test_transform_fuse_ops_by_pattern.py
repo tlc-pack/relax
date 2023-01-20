81c6ec0d34cbbe6e26d5b11612b3fd5dcb1410f2
Masahiro Masuda <masahi129@gmail.com>
2023-01-20 13:16:36 +0900

    clean test

--- tests/python/relax/test_transform_fuse_ops_by_pattern.py
+++ tests/python/relax/test_transform_fuse_ops_by_pattern.py
@@ -39,22 +39,6 @@ class Conv2dReLUx2:
 
 
 @tvm.script.ir_module
-class Conv2dConv2dReLU:
-    @R.function
-    def main(
-        data: R.Tensor((1, 64, 56, 56), "float32"),
-        weight1: R.Tensor((64, 64, 3, 3), "float32"),
-        weight2: R.Tensor((64, 64, 3, 3), "float32"),
-    ):
-        with R.dataflow():
-            conv1 = relax.op.nn.conv2d(data, weight1, padding=(1, 1))
-            conv2d = relax.op.nn.relu(relax.op.nn.conv2d(conv1, weight2, padding=(0, 0)))
-            R.output(conv2d)
-
-        return conv2d
-
-
-@tvm.script.ir_module
 class Conv2dReLUx2Partitioned:
     @R.function
     def main(
@@ -97,50 +81,6 @@ class Conv2dReLUx2Partitioned:
                 conv1, weight21, padding=[0, 0, 0, 0]
             )
             gv2: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.relu(lv2)
-            R.output(gv2)
-        return gv2
-
-
-@tvm.script.ir_module
-class Conv2dConv2dReLUPartitioned:
-    @R.function
-    def main(
-        data: R.Tensor((1, 64, 56, 56), dtype="float32"),
-        weight1: R.Tensor((64, 64, 3, 3), dtype="float32"),
-        weight2: R.Tensor((64, 64, 3, 3), dtype="float32"),
-    ) -> R.Tensor((1, 64, 54, 54), dtype="float32"):
-        with R.dataflow():
-            lv: R.Tensor((1, 64, 56, 56), dtype="float32") = fused_relax_nn_conv2d(data, weight1)
-            gv: R.Tensor((1, 64, 54, 54), dtype="float32") = fused_relax_nn_conv2d_relax_nn_relu(
-                lv, weight2
-            )
-            R.output(gv)
-        return gv
-
-    @R.function
-    def fused_relax_nn_conv2d_relax_nn_relu(
-        conv1: R.Tensor((1, 64, 56, 56), dtype="float32"),
-        weight21: R.Tensor((64, 64, 3, 3), dtype="float32"),
-    ) -> R.Tensor((1, 64, 54, 54), dtype="float32"):
-        R.func_attr({"Primitive": 1, "Composite": "dnnl.conv2d_relu"})
-        with R.dataflow():
-            lv1: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.conv2d(
-                conv1, weight21, padding=[0, 0, 0, 0]
-            )
-            gv1: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.relu(lv1)
-            R.output(gv1)
-        return gv1
-
-    @R.function
-    def fused_relax_nn_conv2d(
-        data1: R.Tensor((1, 64, 56, 56), dtype="float32"),
-        weight11: R.Tensor((64, 64, 3, 3), dtype="float32"),
-    ) -> R.Tensor((1, 64, 56, 56), dtype="float32"):
-        R.func_attr({"Primitive": 1, "Composite": "dnnl.conv2d"})
-        with R.dataflow():
-            gv2: R.Tensor((1, 64, 56, 56), dtype="float32") = R.nn.conv2d(
-                data1, weight11, padding=[1, 1, 1, 1]
-            )
             R.output(gv2)
         return gv2
 
@@ -189,6 +129,66 @@ class Conv2dReLUx2Partitioned_only_conv2d:
 
 
 @tvm.script.ir_module
+class Conv2dConv2dReLU:
+    @R.function
+    def main(
+        data: R.Tensor((1, 64, 56, 56), "float32"),
+        weight1: R.Tensor((64, 64, 3, 3), "float32"),
+        weight2: R.Tensor((64, 64, 3, 3), "float32"),
+    ):
+        with R.dataflow():
+            conv1 = relax.op.nn.conv2d(data, weight1, padding=(1, 1))
+            conv2d = relax.op.nn.relu(relax.op.nn.conv2d(conv1, weight2, padding=(0, 0)))
+            R.output(conv2d)
+
+        return conv2d
+
+
+@tvm.script.ir_module
+class Conv2dConv2dReLUPartitioned:
+    @R.function
+    def main(
+        data: R.Tensor((1, 64, 56, 56), dtype="float32"),
+        weight1: R.Tensor((64, 64, 3, 3), dtype="float32"),
+        weight2: R.Tensor((64, 64, 3, 3), dtype="float32"),
+    ) -> R.Tensor((1, 64, 54, 54), dtype="float32"):
+        with R.dataflow():
+            lv: R.Tensor((1, 64, 56, 56), dtype="float32") = fused_relax_nn_conv2d(data, weight1)
+            gv: R.Tensor((1, 64, 54, 54), dtype="float32") = fused_relax_nn_conv2d_relax_nn_relu(
+                lv, weight2
+            )
+            R.output(gv)
+        return gv
+
+    @R.function
+    def fused_relax_nn_conv2d_relax_nn_relu(
+        conv1: R.Tensor((1, 64, 56, 56), dtype="float32"),
+        weight21: R.Tensor((64, 64, 3, 3), dtype="float32"),
+    ) -> R.Tensor((1, 64, 54, 54), dtype="float32"):
+        R.func_attr({"Primitive": 1, "Composite": "dnnl.conv2d_relu"})
+        with R.dataflow():
+            lv1: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.conv2d(
+                conv1, weight21, padding=[0, 0, 0, 0]
+            )
+            gv1: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.relu(lv1)
+            R.output(gv1)
+        return gv1
+
+    @R.function
+    def fused_relax_nn_conv2d(
+        data1: R.Tensor((1, 64, 56, 56), dtype="float32"),
+        weight11: R.Tensor((64, 64, 3, 3), dtype="float32"),
+    ) -> R.Tensor((1, 64, 56, 56), dtype="float32"):
+        R.func_attr({"Primitive": 1, "Composite": "dnnl.conv2d"})
+        with R.dataflow():
+            gv2: R.Tensor((1, 64, 56, 56), dtype="float32") = R.nn.conv2d(
+                data1, weight11, padding=[1, 1, 1, 1]
+            )
+            R.output(gv2)
+        return gv2
+
+
+@tvm.script.ir_module
 class BranchTupleOutput:
     @R.function
     def main(
@@ -218,8 +218,8 @@ class BranchTupleOutputPartitioned:
                 R.Tensor((1, 64, 54, 54), dtype="float32"),
                 R.Tensor((1, 64, 54, 54), dtype="float32"),
             ) = fused_relax_nn_conv2d_relax_nn_relu(data, weight)
-            lv1: R.Tensor((1, 64, 54, 54), dtype="float32") = lv[1] # conv1
-            lv2: R.Tensor((1, 64, 54, 54), dtype="float32") = lv[0] # relu(conv1)
+            lv1: R.Tensor((1, 64, 54, 54), dtype="float32") = lv[1]  # conv1
+            lv2: R.Tensor((1, 64, 54, 54), dtype="float32") = lv[0]  # relu(conv1)
             gelu1: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.gelu(lv2)
             gelu2: R.Tensor((1, 64, 54, 54), dtype="float32") = R.nn.gelu(lv1)
             out: R.Tensor((1, 64, 54, 54), dtype="float32") = R.add(gelu1, gelu2)
@@ -268,7 +268,7 @@ def test_partition_order():
     tvm.ir.assert_structural_equal(partitioned, Conv2dReLUx2Partitioned_only_conv2d)
 
 
-def test_branch():
+def test_branch_tuple_output():
     partitioned = relax.transform.FuseOpsByPattern([("dnnl.conv2d_relu", conv2d_relu_pat)])(
         BranchTupleOutput
     )
